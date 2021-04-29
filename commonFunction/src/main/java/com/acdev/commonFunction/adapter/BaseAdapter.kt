4948862ee3fb29a950e863f10bcc15373a11b8a9
Ahmad Dody <ahmad.ch@students.amikom.ac.id>
2021-04-29 08:54:23 +0700

    Update BaseAdapter.kt

--- commonFunction/src/main/java/com/acdev/commonFunction/adapter/BaseAdapter.kt
+++ commonFunction/src/main/java/com/acdev/commonFunction/adapter/BaseAdapter.kt
@@ -9,22 +9,20 @@ import androidx.viewbinding.ViewBinding
 import com.acdev.commonFunction.common.InflateFr
 import com.acdev.commonFunction.util.Functionx.Companion.useCurrentTheme
 
-abstract class BaseAdapter<VB : ViewBinding>(private val InflateFr: InflateFr<VB>, private val list: List<Any>) :
-    RecyclerView.Adapter<BaseAdapter.ViewHolder>() {
+abstract class BaseAdapterNew<VB : ViewBinding>(private val InflateFr: InflateFr<VB>, private val list: List<Any>) :
+    RecyclerView.Adapter<BaseAdapterNew.ViewHolder<VB>>() {
 
-    private var _binding: VB? = null
-    val binding get() = _binding!!
     val gone: Int = View.GONE
     val visible: Int = View.VISIBLE
     val invisible: Int = View.INVISIBLE
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder<VB> {
         parent.context.useCurrentTheme()
-        _binding = InflateFr.invoke((parent.context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE) as LayoutInflater), parent, false)
-        return ViewHolder(binding.root)
+        val binding = InflateFr.invoke((parent.context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE) as LayoutInflater), parent, false)
+        return ViewHolder(binding)
     }
 
     override fun getItemCount() = list.size
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class ViewHolder<VB: ViewBinding>(val binding: VB) : RecyclerView.ViewHolder(binding.root)
 }
